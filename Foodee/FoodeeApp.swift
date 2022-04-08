7b46bd52d516271e072b2aa42943c46e749da873
user210897 <user210897@74-80-245-230.ic2net.net>
2022-04-08 00:42:47 -0700

    intial commit & adding stater code

---
+++ Foodee/FoodeeApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct FoodeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PermissionView()
+        }
+    }
+}
---
+++ Foodee/Helpers/Font.swift
@@ -0,0 +1,9 @@
+
+
+import Foundation
+
+extension String {
+    static let poppinsRegular = "Poppins-Regular"
+    static let poppinsMedium = "Poppins-Medium"
+    static let poppinsSemibold = "Poppins-SemiBold"
+}
