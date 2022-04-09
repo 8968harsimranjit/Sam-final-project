f1932108486088808224c817b1af897771578ae8
Sam <harsimranchahal170@gmail.com>
2022-04-08 18:07:46 -0700

    adding content to the main(preference) screen

--- Foodee/Views/Permisson/PermissionView.swift
+++ Foodee/Views/Permisson/PermissionView.swift
@@ -4,8 +4,32 @@ import SwiftUI
 
 struct PermissionView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader{ proxy in
+            VStack{
+                
+                ZStack{
+                    Image("food1")
+                }.frame(height:proxy.size.height / 3)
+                
+                Text("Foodee")
+                    .font(.title)
+                
+                Text("Find delicious food near you")
+                    .font(.headline)
+                
+                Spacer()
+                
+                Button(action: {}) {
+                    Text("Get Started")
+                }
+                .padding()
+                .frame(width: proxy.size.width - 50)
+                .background(Color.yellow)
+                .cornerRadius(50)
+                .shadow(radius: 10)
+                .foregroundColor(.white)
+            }
+        }
     }
 }
 
