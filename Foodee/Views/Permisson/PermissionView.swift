5079da00e79b416a17235666b64e25300f508ab7
Sam <harsimranchahal170@gmail.com>
2022-04-10 19:44:47 -0700

    Adding animation on the permission screen

--- Foodee/Views/Permisson/PermissionView.swift
+++ Foodee/Views/Permisson/PermissionView.swift
@@ -3,12 +3,27 @@
 import SwiftUI
 
 struct PermissionView: View {
+    
+    @State var isAnimating = false
     var body: some View {
         GeometryReader{ proxy in
             VStack{
                 
                 ZStack{
-                    Image("food1")
+                    ForEach(1..<14){i in             // adding ForEach loop to loop                                      over all the images.
+                        Image("food\(i % 7)")
+                            .position(
+                                x: .random(in: 0...proxy.size.width),
+                                y: .random(in: 0...proxy.size.height / 2)
+                            )
+                            .animation(
+                                    .interpolatingSpring(stiffness: 0.5, damping: 0.5)
+                                    .repeatForever()
+                                    .delay(isAnimating ? .random(in: 0...1): 0)
+                                    .speed(isAnimating ? .random(in: 0...1): 0)
+                            )
+                    }
+                    
                 }.frame(height:proxy.size.height / 3)
                 
                 Text("Foodee")
@@ -29,6 +44,8 @@ struct PermissionView: View {
                 .shadow(radius: 10)
                 .foregroundColor(.white)
             }
+        }.onAppear{
+            isAnimating.toggle()
         }
     }
 }
